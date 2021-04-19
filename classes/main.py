4546d63b2d30a506adf63aa0055b7ee1de14e826
igor.maculewicz <igor.maculewicz@assecods.pl>
2021-04-19 16:48:59 +0200

    Added scrapping algorithm

--- classes/main.py
+++ classes/main.py
@@ -1,26 +1,4 @@
-def generator(start_value=0):
-    while start_value < 1_000_000_000_000:
-        start_value += 2
-        yield start_value
+import calendar
 
-
-def funkcja(start_value=0):
-    lista = []
-    while start_value < 1_000_000_000_000:
-        start_value += 2
-        lista.append(start_value)
-    return lista
-
-
-for x in generator():
-    print(f"Uzycie generatora:{x}")
-
-# for x in funkcja():
-#     print(f"Uzycie funkcji: {x}")
-
-
-for x in generator():
-    print(f"Uzycie generatora:{x}")
-
-# for x in funkcja():
-#     print(f"Uzycie funkcji: {x}")
+import calendar
+print(calendar.weekheader(100))
---
+++ test_package/test_subpackage/test_module.py
@@ -0,0 +1,5 @@
+def test_function():
+    if __name__ == "__main__":
+        print("I prefer to be a module.")
+    else:
+        print("I like to be a module.")
